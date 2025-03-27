b4aea4db0ffd28f193289d3e1f16fcce8a528e8b
Cem Demirtas <140912930+cemgamedev@users.noreply.github.com>
2025-03-27 18:57:08 +0300

    [Add] Anchored on board

--- Assets/Scripts/ItemSpawner.cs
+++ Assets/Scripts/ItemSpawner.cs
@@ -1,6 +1,4 @@
-// maebleme2
-
-using System;
+﻿using System;
 using System.Collections.Generic;
 using DG.Tweening;
 using Ebleme;
@@ -11,105 +9,156 @@ using StickBlast.Models;
 using UniRx;
 using StickBlast.Core.Interfaces;
 using StickBlast.Implementation;
-
 namespace DefaultNamespace
 {
-    public interface IItemSpawner
-    {
-        void HandleItemSpawn();
-        void HandleItemDestruction(Item item);
-    }
+	public interface IItemSpawner
+	{
+		void HandleItemSpawn();
+		void HandleItemDestruction(Item item);
+	}
+	public class ItemSpawner : Singleton<ItemSpawner>, IItemSpawner
+	{
+		[SerializeField]
+		private Transform spawnPoint;
+		[SerializeField]
+		private Transform[] itemPoints;
+		[SerializeField]
+		private Transform boardTransform;
+		private List<Item> items;
+		private IItemAnimationController animationController;
+		private CompositeDisposable disposables = new CompositeDisposable();
 
-    public class ItemSpawner : Singleton<ItemSpawner>, IItemSpawner
-    {
-        [SerializeField]
-        private Transform spawnPoint;
+		private void Awake()
+		{
+			animationController = new ItemAnimationController();
+			((ItemAnimationController)animationController).HandleInitialization();
+			((ItemAnimationController)animationController).SetSpawnPoint(spawnPoint.position);
+		}
 
-        [SerializeField]
-        private Transform[] itemPoints;
+		private void OnDestroy()
+		{
+			disposables.Dispose();
+		}
 
-        private List<Item> items;
-        private IItemAnimationController animationController;
-        private CompositeDisposable disposables = new CompositeDisposable();
+		private void Start()
+		{
+			if (CommonGameAssets.Instance == null)
+			{
+				Debug.LogError("CommonGameAssets is not initialized!");
+				return;
+			}
+			HandleItemSpawn();
+		}
 
-        private void Awake()
-        {
-            animationController = new ItemAnimationController();
-            ((ItemAnimationController)animationController).HandleInitialization();
-            ((ItemAnimationController)animationController).SetSpawnPoint(spawnPoint.position);
-        }
+		// Helper method to get the main SpriteRenderer
+		private SpriteRenderer GetMainSpriteRenderer(GameObject itemPrefab)
+		{
+			// First try to get the main SpriteRenderer on the prefab
+			SpriteRenderer mainRenderer = itemPrefab.GetComponent<SpriteRenderer>();
 
-        private void OnDestroy()
-        {
-            disposables.Dispose();
-        }
+			// If not found, look in children
+			if (mainRenderer == null)
+			{
+				mainRenderer = itemPrefab.GetComponentInChildren<SpriteRenderer>();
+			}
 
-        private void Start()
-        {
-            if (CommonGameAssets.Instance == null)
-            {
-                Debug.LogError("CommonGameAssets is not initialized!");
-                return;
-            }
-            HandleItemSpawn();
-        }
+			return mainRenderer;
+		}
 
-        public void HandleItemSpawn()
-        {
-            if (CommonGameAssets.Instance == null)
-            {
-                Debug.LogError("CommonGameAssets is not initialized!");
-                return;
-            }
+		public void HandleItemSpawn()
+		{
+			if (CommonGameAssets.Instance == null)
+			{
+				Debug.LogError("CommonGameAssets is not initialized!");
+				return;
+			}
+			var itemPrefabs = CommonGameAssets.Instance.GetRandomItems();
+			if (itemPrefabs == null || itemPrefabs.Count == 0)
+			{
+				Debug.LogError("No item prefabs available!");
+				return;
+			}
+			items = new List<Item>();
 
-            var itemPrefabs = CommonGameAssets.Instance.GetRandomItems();
-            if (itemPrefabs == null || itemPrefabs.Count == 0)
-            {
-                Debug.LogError("No item prefabs available!");
-                return;
-            }
+			// Get board boundaries
+			Renderer boardRenderer = boardTransform.GetComponent<Renderer>();
+			if (boardRenderer == null)
+			{
+				Debug.LogError("Board does not have a Renderer component!");
+				return;
+			}
 
-            items = new List<Item>();
+			Vector3 boardMin = boardRenderer.bounds.min;
+			Vector3 boardMax = boardRenderer.bounds.max;
 
-            foreach (var itemPrefab in itemPrefabs)
-            {
-                if (itemPrefab == null) continue;
+			// Use the first itemPoint's Y position to ensure consistent Y alignment
+			float spawnY = itemPoints[0].position.y;
 
-                var item = Instantiate(itemPrefab);
-                item.transform.position = spawnPoint.position;
-                
-                // Subscribe to item destruction using UniRx
-                item.OnItemDestroyed
-                    .Subscribe(HandleItemDestruction)
-                    .AddTo(disposables);
-                
-                items.Add(item);
-            }
+			// Calculate spacing and start position based on board size
+			float boardWidth = boardMax.x - boardMin.x;
 
-            animationController.HandleItemAnimation(items, itemPoints)
-                .Subscribe(_ =>
-                {
-                    foreach (var item in items)
-                        item.SetCanTouch();
-                })
-                .AddTo(disposables);
-        }
+			// Get the main SpriteRenderer of the first prefab to calculate size
+			SpriteRenderer firstItemRenderer = GetMainSpriteRenderer(itemPrefabs[0].gameObject);
+			if (firstItemRenderer == null)
+			{
+				Debug.LogError("Could not find SpriteRenderer for item!");
+				return;
+			}
 
-        public void HandleItemDestruction(Item item)
-        {
-            if (items.Contains(item))
-            {
-                items.Remove(item);
+			float itemWidth = firstItemRenderer.bounds.size.x;
+			float spacing = itemWidth * 1.2f; // Add a little padding
+			float startX = boardMin.x + (itemWidth / 2);
 
-                if (items.Count == 0)
-                {
-                    HandleItemSpawn();
-                }
-            }
-            else
-            {
-                Debug.LogError("Item should be in the list");
-            }
-        }
-    }
+			foreach (var itemPrefab in itemPrefabs)
+			{
+				if (itemPrefab == null) continue;
+
+				// Calculate X position
+				float spawnX = startX + (items.Count * spacing);
+
+				// Check if the item will be within board boundaries
+				if (spawnX + (itemWidth / 2) > boardMax.x)
+				{
+					Debug.LogWarning("Too many items to spawn within board boundaries!");
+					break;
+				}
+
+				var item = Instantiate(itemPrefab);
+
+				// Set position with the consistent Y and constrained X
+				item.transform.position = new Vector3(spawnX, spawnY, itemPoints[0].position.z);
+
+				// Subscribe to item destruction using UniRx
+				item.OnItemDestroyed
+					.Subscribe(HandleItemDestruction)
+					.AddTo(disposables);
+
+				items.Add(item);
+			}
+
+			animationController.HandleItemAnimation(items, itemPoints)
+				.Subscribe(_ =>
+				{
+					foreach (var item in items)
+						item.SetCanTouch();
+				})
+				.AddTo(disposables);
+		}
+
+		public void HandleItemDestruction(Item item)
+		{
+			if (items.Contains(item))
+			{
+				items.Remove(item);
+				if (items.Count == 0)
+				{
+					HandleItemSpawn();
+				}
+			}
+			else
+			{
+				Debug.LogError("Item should be in the list");
+			}
+		}
+	}
 }
